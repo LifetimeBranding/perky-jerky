02a4a225a0ca451f08f490910b56782f4b24cd8c
Phil Fevry <pf@phlfvry.com>
2017-10-04 13:20:24 -0400

    started main menu slider

--- assets/pj.js
+++ assets/pj.js
@@ -1,4 +1,5 @@
 var pj, p
+var pj_navmenu
 
 // MAIN CONTROLLER
 pj = {
@@ -11,6 +12,15 @@ pj = {
   bindActions: function() {
     //
   },
+}
+
+pj_navmenu = {
+  toggleMenu: function() {
+    let menuSlideOut = $('.navigation__main-menu--slide-out');
+    let currentWidth = menuSlideOut.css('width');
+    console.log(currentWidth);
+    (currentWidth === "0px") ? menuSlideOut.css('width', '150px') : menuSlideOut.css('width', '0px');
+  }
 }
 
 // DOCUMENT LOAD
